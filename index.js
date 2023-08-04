e474d1ab1d664f3ac50ab192e7b87ada591f1a27
ShaliniSinha35 <shalinisinha3560@gmail.com>
2023-08-04 13:42:21 +0530

    commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path=require("path")
 app.use(cors());
 const products=require("./product.json")
 const port = process.env.PORT || 5000;
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
 // products
 app.use('/images/product', express.static(path.resolve(__dirname,'/images/product')));
