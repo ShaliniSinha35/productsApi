0c9556f88681e2f8df61667e0d4cddc7ce4ef9d9
ShaliniSinha35 <shalinisinha3560@gmail.com>
2023-08-04 13:45:34 +0530

    commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(express.static('public'));
 
 // products
-app.use('/images/product', express.static(path.resolve(__dirname,'/images/product')));
+app.use('/public/images/product', express.static(path.resolve(__dirname,'/public/images/product')));
 app.get('/', (req, res) => {
   const newData = products.map(item =>{
     return {
