6e8fa3064f1b55fdd75553a90a1199fbe33700b9
ShaliniSinha35 <shalinisinha3560@gmail.com>
2023-08-05 17:36:22 +0530

    commit

--- index.js
+++ index.js
@@ -1,45 +1,43 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const path=require("path");
+const path = require("path");
 
 app.use(cors());
-const products=require("./products.json");
-const logo=require("./logo.json")
+const products = require("./products.json");
+const logo = require("./logo.json");
 const port = process.env.PORT || 5000;
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 // products
-// app.use('/public/images/product', express.static('public/images/product'));
-app.use('/images', express.static(path.resolve(__dirname, 'images/product')));
+app.use(
+  "/public/images/product",
+  express.static(path.resolve(__dirname, "public/images/product"))
+);
 
-app.get('/', (req, res) => {
-  const newData = products.map(item =>{
+app.get("/", (req, res) => {
+  const newData = products.map((item) => {
     return {
       ...item,
-      img: `${req.protocol}://${req.get('host')}${item.images[0]}` //  full image URL
- };
+      img: `${req.protocol}://${req.get("host")}${item.images[0]}`, //  full image URL
+    };
   });
   return res.status(200).json(newData);
- 
-
-
 });
 
 // logo
-app.use('/public/images/logo', express.static(path.resolve(__dirname,'public/images/logo')));
-app.get('/logo', (req, res) => {
-  const newData = logo.map(item =>{
+app.use(
+  "/public/images/logo",
+  express.static(path.resolve(__dirname, "public/images/logo"))
+);
+app.get("/logo", (req, res) => {
+  const newData = logo.map((item) => {
     return {
       ...item,
-      img: `${req.protocol}://${req.get('host')}${item.img}` //  full image URL
- };
+      img: `${req.protocol}://${req.get("host")}${item.img}`, //  full image URL
+    };
   });
   return res.status(200).json(newData);
- 
-
-
 });
 
 app.listen(port, () => {
@@ -47,6 +45,3 @@ app.listen(port, () => {
 });
 
 // http://products-api-pearl.vercel.app/images/product/tomato.jpg
-
-
-
